a37b233de97efd6eabaa74b2fe4f62e1b3f111bb
AvraamNikolaou <avnikolaou@hotmail.com>
2020-09-04 19:52:53 +0300

    feat: comment moderation service

--- query/index.js
+++ query/index.js
@@ -19,14 +19,14 @@ app.post('/events', (req, res) => {
     if (type === 'PostCreated') {
         const { id, title } = data;
 
-        posts[id] = {id, title, comments: [] };
+        posts[id] = { id, title, comments: [] };
     }
 
     if (type === 'CommentCreated') {
-        const { id, content, postId } = data;
+        const { id, content, postId, status } = data;
 
         const post = posts[postId];
-        post.comments.push({ id, content });
+        post.comments.push({ id, content, status });
     }
 
     console.log(posts);
